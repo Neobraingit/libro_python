0199d7ad6fc90449f8cd380d18b659391ad5d8c0
Neobraingit <Maervca2@icloud.com>
2023-05-15 19:53:27 +0200

    números

---
+++ numeros.py
@@ -0,0 +1,24 @@
+# Operaciones con números enteros
+suma = 1 + 1
+resta = 1 - 1
+division = 1 / 1
+multiplicacion = 1 * 1
+print (suma)
+print (resta)
+print (division)
+print (multiplicacion)
+
+# Numeros flotantes
+suma = 1.1 + 1.1
+resta = 1.1 - 1.1
+division = 1.1 / 1.1
+multiplicacion = 1.1 * 1.1
+print (suma)
+print (resta)
+print (division)
+print (multiplicacion)
+
+# Guiones en números largos
+numero_largo = 1_000_000
+print (numero_largo) # Solo imprimirá el número sin guiones
+
