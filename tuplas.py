e4bd9b41357e81aa35c905cbf80f292b0605f42e
Neobraingit <Maervca2@icloud.com>
2023-05-17 18:19:19 +0200

    tuplas

---
+++ tuplas.py
@@ -0,0 +1,11 @@
+# Definir una tupla
+tupla = (20, 89, 70)
+
+# Acceder a los elementos de una tupla
+print (tupla[0])
+
+# Bucle for en una tupla
+coches = ('seat', 'peugeot', 'nissan', 'citroen')
+for i in coches:
+    print (i)
+    
